4061e5117caf52b2194e24c482b1c56080eb491b
Bruno Kambere <brunokambere@gmail.com>
2025-07-25 02:00:56 +0300

    [ENH] Migration: Update stores table migration to  add deleted_at and deleted_by columns

--- app/Migrations/20250725_create_stores_table.php
+++ app/Migrations/20250725_create_stores_table.php
@@ -4,9 +4,9 @@ namespace App\Migrations;
 use Core\Database;
 use Core\MigrationInterface;
 
-class CreateStoresTable implements MigrationInterface
+class CreateStoresTable implements MigrationInterface 
 {
-    public function up(): void
+    public function up(): void 
     {
         $db = Database::getInstance()->pdo();
         $sql = <<<'SQL'
@@ -14,22 +14,23 @@ CREATE TABLE IF NOT EXISTS stores (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL UNIQUE,
   slug TEXT NOT NULL UNIQUE,
-  address_line1 TEXT NOT NULL,
+  address_line1 TEXT,
   address_line2 TEXT,
-  city TEXT NOT NULL,
-  state_region TEXT NOT NULL,
-  country TEXT NOT NULL,
+  city TEXT,
+  state_region TEXT,
+  country TEXT,
   phone TEXT,
   email TEXT,
   created_at TEXT NOT NULL DEFAULT (datetime('now')),
-  updated_at TEXT NOT NULL DEFAULT (datetime('now'))
+  updated_at TEXT NOT NULL DEFAULT (datetime('now')),
+  deleted_at TEXT,
+  deleted_by INTEGER,
+  FOREIGN KEY(deleted_by) REFERENCES users(id)
 );
 SQL;
         $db->exec($sql);
     }
-
-    public function down(): void
-    {
+    public function down(): void {
         Database::getInstance()->pdo()->exec("DROP TABLE IF EXISTS stores");
     }
 }
