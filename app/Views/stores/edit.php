155d2d97dcd0ff3cac47c0afd003216ef2a3c7f2
Bruno Kambere <brunokambere@gmail.com>
2025-07-26 01:56:27 +0300

    [UI] Styles: Enhance edit store form layout and structure for improved usability

--- app/Views/stores/edit.php
+++ app/Views/stores/edit.php
@@ -1,14 +1,34 @@
 <h1>Edit Store</h1>
-<form method="post" action="/stores/update/<?= $store->id ?>">
-    <?= $this->csrfField() ?>
-    <label>Name: <input name="name" value="<?= htmlspecialchars($store->name) ?>"></label><br>
-    <label>Slug: <input name="slug" value="<?= htmlspecialchars($store->slug) ?>"></label><br>
-    <label>Address Line 1: <input name="address_line1" value="<?= htmlspecialchars($store->address_line1) ?>"></label><br>
-    <label>Address Line 2: <input name="address_line2" value="<?= htmlspecialchars($store->address_line2) ?>"></label><br>
-    <label>City: <input name="city" value="<?= htmlspecialchars($store->city) ?>"></label><br>
-    <label>State/Region: <input name="state_region" value="<?= htmlspecialchars($store->state_region) ?>"></label><br>
-    <label>Country: <input name="country" value="<?= htmlspecialchars($store->country) ?>"></label><br>
-    <label>Phone: <input name="phone" value="<?= htmlspecialchars($store->phone) ?>"></label><br>
-    <label>Email: <input name="email" value="<?= htmlspecialchars($store->email) ?>"></label><br>
-    <button type="submit">Update</button>
+<form class="form-grid" method="post" action="/stores/update/<?= $store->id ?>">
+    <div class="form-group">
+        <?= $this->csrfField() ?>
+        <label for="name">Name:</label>
+        <input id="name" name="name" value="<?= htmlspecialchars($store->name) ?>" required>
+
+        <label for="slug">Slug:</label>
+        <input id="slug" name="slug" value="<?= htmlspecialchars($store->slug) ?>" required>
+
+        <label for="address_line1">Address Line 1:</label>
+        <input id="address_line1" name="address_line1" value="<?= htmlspecialchars($store->address_line1) ?>" required>
+
+        <label for="address_line2">Address Line 2:</label>
+        <input id="address_line2" name="address_line2" value="<?= htmlspecialchars($store->address_line2) ?>">
+
+        <label for="city">City:</label>
+        <input id="city" name="city" value="<?= htmlspecialchars($store->city) ?>" required>
+
+        <label for="state_region">State/Region:</label>
+        <input id="state_region" name="state_region" value="<?= htmlspecialchars($store->state_region) ?>">
+
+        <label for="country">Country:</label>
+        <input id="country" name="country" value="<?= htmlspecialchars($store->country) ?>">
+
+        <label for="phone">Phone:</label>
+        <input id="phone" name="phone" value="<?= htmlspecialchars($store->phone) ?>">
+
+        <label for="email">Email:</label>
+        <input id="email" name="email" value="<?= htmlspecialchars($store->email) ?>">
+        
+        <button type="submit">Update</button>
+    </div>
 </form>
